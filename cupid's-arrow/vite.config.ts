7efb3a05a566905babebb3aa3ec06afbccb4621f
Jas?Yeah <n.saijaswin.2007@gmail.com>
2026-01-30 17:46:53 +0530

    vite.config.ts
    Bug fix - Fixed E404

--- cupid's-arrow/vite.config.ts
+++ cupid's-arrow/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Add this line so GitHub Pages knows where to find your files
+      base: '/CupidsArrowV1/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
